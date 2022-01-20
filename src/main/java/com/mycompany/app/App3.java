070caea1f21e6ea58eea7adb2bc6c5970271616c
Emilio Kenji Hernández Kuramata <emilio.her.kur@gmail.com>
2022-01-20 16:58:22 +0100

    Probando Checkout

---
+++ src/main/java/com/mycompany/app/App3.java
@@ -0,0 +1,8 @@
+package com.mycompany.app;
+
+public class App3 {
+    public static void main( String[] args )
+    {
+        System.out.println( "Estoy en la rama backup pero NO en main" );
+    }
+}
